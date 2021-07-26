f532b586a6d590cfa2fb37e139d7703b3ae18df3
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-25 19:50:49 -0700

    fixed weather and forecast function by using jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 const apiKey = "623e9032c9f9cf85d8db90c25579c5ff";
 const history = localStorage.getItem("searchHistory") || [];
 const currentWeather = (searchInput) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`
-    const weather = $.get(url, function(data){
-        console.log(data)
-    })
-}
-// 5 day forecast
-api.openweathermap.org/data/2.5/forecast?q={city name}&appid=623e9032c9f9cf85d8db90c25579c5ff 
- 
-// current
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`;
+  $.get(url, function (data) {
+    console.log(data);
+  });
+};
+
+const futureForecast = (searchInput) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${apiKey}`;
+  $.get(url, function (data) {
+    console.log(data);
+  });
+};
